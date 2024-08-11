816caf18959eea9e498bce892b4dcd9c8e14198e
motoshifurugen <amanogawa_m@icloud.com>
2024-08-11 22:51:50 +0900

    :alembic: Github Pages公開用の設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/my_site',
+  assetPrefix: '/my_site/',
+};
 
 export default nextConfig;
