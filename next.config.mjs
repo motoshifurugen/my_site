d41d71fe329fcb466353c1105c51fc606b3de8e2
motoshifurugen <amanogawa_m@icloud.com>
2024-08-11 23:15:24 +0900

    :rotating_light: imagesのpath修正

--- next.config.mjs
+++ next.config.mjs
@@ -7,5 +7,12 @@ const nextConfig = {
   basePath: '/my_site',
   assetPrefix: '/my_site/',
 };
+module.exports = {
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/my-site/' : '',
+  images: {
+    loader: 'imgix',
+    path: '',
+  },
+};
 
 export default nextConfig;
