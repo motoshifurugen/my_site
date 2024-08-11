804722ba0d904601ceb24957ab960870d975477a
motoshifurugen <amanogawa_m@icloud.com>
2024-08-12 00:07:00 +0900

    :bug: フォントパス修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import "./globals.css";
 import 'tailwindcss/tailwind.css';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import assetPrefix from "../../next.config.mjs";
+import Head from "next/head"; // Import the Head component
 
 config.autoAddCss = false
 
@@ -23,8 +25,31 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const basePath = process.env.NODE_ENV === 'production' ? assetPrefix : '';
   return (
     <html lang="ja">
+      <Head>
+        <style>{`
+          @font-face {
+            font-family: 'DMSans';
+            src: url('${basePath}/fonts/DMSans-VariableFont.ttf') format('truetype');
+            font-weight: normal;
+            font-style: normal;
+          }
+          @font-face {
+            font-family: 'OpenSans';
+            src: url('${basePath}/fonts/OpenSans-VariableFont.ttf') format('truetype');
+            font-weight: normal;
+            font-style: normal;
+          }
+          @font-face {
+            font-family: 'MOBO';
+            src: url('${basePath}/fonts/MOBO-SemiBold.otf') format('truetype');
+            font-weight: normal;
+            font-style: normal;
+          }
+        `}</style>
+      </Head>
       <body className={inter.className}>
         <BackgroundWrapper>
           <Header />
