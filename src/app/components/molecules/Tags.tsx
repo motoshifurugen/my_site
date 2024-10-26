4dba7e91e4f05d787f06a9b85fd3af5c80edc081
motoshifurugen <amanogawa_m@icloud.com>
2024-10-26 09:03:37 +0900

    :sparkles: タグによる絞り込み実装

--- src/app/components/molecules/Tags.tsx
+++ src/app/components/molecules/Tags.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 
 interface TagsProps {
   tags: string[]
+  onClickTag: (tag: string) => void
 }
 
-const Tags: React.FC<TagsProps> = ({ tags }) => {
+const Tags: React.FC<TagsProps> = ({ tags, onClickTag }) => {
   return (
     <div className="mb-16 mt-8 flex flex-wrap gap-2">
       {tags.map((tag: string, index: number) => (
         <span
           key={index}
-          className="inline-block rounded bg-teal px-2.5 py-1.5 text-xs text-main-white"
+          className="inline-block cursor-pointer rounded bg-teal px-2.5 py-1.5 text-xs text-main-white"
+          onClick={() => onClickTag(tag)}
         >
           {tag}
         </span>
