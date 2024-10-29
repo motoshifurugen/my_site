961f4f5e30ed275d7e33e162d17bb19154c11a56
motoshifurugen <amanogawa_m@icloud.com>
2024-10-30 00:31:40 +0900

    :construction_worker: APIルートを出力から除外

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // output: 'export',
+  output: 'export',
   basePath: '/my_site',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/my_site/' : '',
   staticPageGenerationTimeout: 60,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    // APIルートを除外
+    const pathMap = { ...defaultPathMap }
+    delete pathMap['/api/blog/[slug]']
+    return pathMap
+  },
 };
 
 export default nextConfig;
