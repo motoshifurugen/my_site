ecdf5f7e042412c34957a13ae307d656d8ef9232
motoshifurugen <amanogawa_m@icloud.com>
2024-10-30 09:05:19 +0900

    フロントもビルド

--- src/app/(pages)/blog/[slug]/page.tsx
+++ src/app/(pages)/blog/[slug]/page.tsx
@@ -1,85 +1,65 @@
-// import Toc from '@/app/components/molecules/Toc'
-// import ArticleContent from '@/app/components/templates/ArticleContent'
+import Toc from '@/app/components/molecules/Toc'
+import ArticleContent from '@/app/components/templates/ArticleContent'
 
-// type Post = {
-//   slug: string
-//   title: string
-//   date: string
-//   blogContentsHTML: string
-// }
-
-// // SSG
-// export async function generateStaticParams() {
-//   const apiUrl =
-//     process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/my_site/api'
-//   try {
-//     const res = await fetch(`${apiUrl}/blog/`, {
-//       cache: 'force-cache',
-//     })
-//     if (!res.ok) {
-//       const errorText = await res.text()
-//       throw new Error(`Failed to fetch data from ${apiUrl}/blog/: ${errorText}`)
-//     }
-//     const blogData = await res.json()
-//     return blogData.map((blog: Post) => ({
-//       slug: blog.slug,
-//     }))
-//   } catch (error) {
-//     console.error('Error fetching blog data:', error)
-//     throw new Error('Failed to fetch blog data')
-//   }
-// }
-
-// const getBlogArticle = async (slug: string) => {
-//   const apiUrl =
-//     process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/my_site/api'
-//   try {
-//     const res = await fetch(`${apiUrl}/blog/${slug}`, {
-//       cache: 'force-cache',
-//     })
-//     if (!res.ok) {
-//       const errorText = await res.text()
-//       throw new Error(
-//         `Failed to fetch data from ${apiUrl}/blog/${slug}: ${errorText}`,
-//       )
-//     }
-//     const blogArticle = await res.json()
-//     return blogArticle
-//   } catch (error) {
-//     console.error('Error fetching blog article:', error)
-//     throw new Error('Failed to fetch blog article')
-//   }
-// }
-
-// const BlogArticlePage = async ({ params }: { params: { slug: string } }) => {
-//   const blogArticle = await getBlogArticle(params.slug)
-
-//   return (
-//     <section>
-//       <ArticleContent
-//         blogArticle={blogArticle}
-//         SidebarComponents={[<Toc key="toc" />]}
-//       />
-//     </section>
-//   )
-// }
-
-// export default BlogArticlePage
-
-// 仮の静的パラメータを生成
-export async function generateStaticParams() {
-  return [
-    { slug: 'sample-slug-1' },
-    { slug: 'sample-slug-2' },
-    { slug: 'sample-slug-3' },
-  ]
+type Post = {
+  slug: string
+  title: string
+  date: string
+  blogContentsHTML: string
 }
 
-const BlogArticlePage = ({ params }: { params: { slug: string } }) => {
+// SSG
+export async function generateStaticParams() {
+  const apiUrl =
+    process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/my_site/api'
+  try {
+    const res = await fetch(`${apiUrl}/blog/`, {
+      cache: 'force-cache',
+    })
+    if (!res.ok) {
+      const errorText = await res.text()
+      throw new Error(`Failed to fetch data from ${apiUrl}/blog/: ${errorText}`)
+    }
+    const blogData = await res.json()
+    return blogData.map((blog: Post) => ({
+      slug: blog.slug,
+    }))
+  } catch (error) {
+    console.error('Error fetching blog data:', error)
+    throw new Error('Failed to fetch blog data')
+  }
+}
+
+const getBlogArticle = async (slug: string) => {
+  const apiUrl =
+    process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/my_site/api'
+  try {
+    const res = await fetch(`${apiUrl}/blog/${slug}`, {
+      cache: 'force-cache',
+    })
+    if (!res.ok) {
+      const errorText = await res.text()
+      throw new Error(
+        `Failed to fetch data from ${apiUrl}/blog/${slug}: ${errorText}`,
+      )
+    }
+    const blogArticle = await res.json()
+    return blogArticle
+  } catch (error) {
+    console.error('Error fetching blog article:', error)
+    throw new Error('Failed to fetch blog article')
+  }
+}
+
+const BlogArticlePage = async ({ params }: { params: { slug: string } }) => {
+  const blogArticle = await getBlogArticle(params.slug)
+
   return (
     <section>
-      <h1>仮のページ</h1>
-      <p>このページは仮のページです。スラッグ: {params.slug}</p>
+      <ArticleContent
+        blogArticle={blogArticle}
+        SidebarComponents={[<Toc key="toc" />]}
+      />
     </section>
   )
 }
