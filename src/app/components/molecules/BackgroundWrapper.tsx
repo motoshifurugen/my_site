52c5a9a8dfe33d403eaaf14ddbb07ee72f0f2145
motoshifurugen <amanogawa_m@icloud.com>
2024-11-13 01:00:06 +0900

    :zap: スマホ画面ではアニメーションコンポーネントを利用しない

--- src/app/components/molecules/BackgroundWrapper.tsx
+++ src/app/components/molecules/BackgroundWrapper.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import ParticleBackground from '@/app/components/atoms/ParticlesBackground'
+// import ParticleBackground from '@/app/components/atoms/ParticlesBackground'
 import SketchCloud from '@/app/components/atoms/SketchCloud'
 import { usePathname } from 'next/navigation'
 import React from 'react'
@@ -17,17 +17,21 @@ const BackgroundWrapper: React.FC<{ children: React.ReactNode }> = ({
     <div className="relative min-h-screen">
       {isRootPath && (
         <>
-          <div className="min-h-screen"></div>
+          <div className="hidden md:block md:min-h-screen"></div>
           <div className="fixed inset-0 z-0 hidden md:block">
             <SketchCloud mode="normal" />
           </div>
           <div className="fixed inset-0 z-0 md:hidden">
-            <ParticleBackground />
+            {/* <ParticleBackground /> */}
+            <div
+              className="absolute inset-0"
+              style={{ backgroundColor: '#86b3e0' }}
+            ></div>
           </div>
         </>
       )}
       <div
-        className={`${isRootPath ? 'relative z-10 pb-32' : ''}`}
+        className={`${isRootPath ? 'relative z-10 min-h-screen pb-32' : ''}`}
         style={{
           backgroundColor: isRootPath
             ? 'rgba(246, 246, 246, 0.5)'
